8e2af63335ce32badf58b833c6b3f3740cdd7a9f
arpieb <rob@octobang.com>
2021-05-07 17:50:10 -0400

    #8: WIP; added Nixa.Distance to provide common distance metrics for real and integer vectors

---
+++ lib/nixa/distance.ex
@@ -0,0 +1,44 @@
+defmodule Nixa.Distance do
+  @moduledoc """
+  Implements various distance metrics
+  """
+
+  import Nx.Defn
+
+  defn euclidean(x, y) do
+    Nx.sqrt(Nx.sum(Nx.power(x - y, 2)))
+  end
+
+  defn manhattan(x, y) do
+    Nx.sum(Nx.abs(x - y))
+  end
+
+  defn chebyshev(x, y) do
+    Nx.reduce_max(Nx.abs(x - y))
+  end
+
+  defn minkowski(x, y, p) do
+    Nx.power(Nx.sum(Nx.power(Nx.abs(x - y), p)), 1 / p)
+  end
+
+  defn wminkowski(x, y, p, w) do
+    Nx.power(Nx.sum(Nx.power(Nx.abs(w * (x - y)), p)), 1 / p)
+  end
+
+  defn seuclidean(x, y, v) do
+    Nx.sqrt(Nx.sum(Nx.power(x - y, 2)) / v)
+  end
+
+  defn hamming(x, y) do
+    Nx.select(Nx.not_equal(x, y), 1, 0) |> Nx.sum() |> Nx.divide(max(Nx.size(x), Nx.size(y)))
+  end
+
+  defn canberra(x, y) do
+    Nx.sum(Nx.abs(x - y) / (Nx.abs(x) + Nx.abs(y)))
+  end
+
+  defn braycurtis(x, y) do
+    Nx.sum(Nx.abs(x - y) / (Nx.sum(Nx.abs(x)) + Nx.sum(Nx.abs(y))))
+  end
+
+end
---
+++ test/distance_test.exs
@@ -0,0 +1,69 @@
+defmodule NixaTest.Distance do
+  use ExUnit.Case
+  doctest Nixa
+
+  @delta 1.0e-6
+
+  import Nixa.Distance
+
+  test "euclidean" do
+    {x, y} = get_xy()
+    d = euclidean(x, y) |> Nx.to_scalar()
+    assert_in_delta(d, 5.196152210235596, @delta)
+  end
+
+  test "manhattan" do
+    {x, y} = get_xy()
+    d = manhattan(x, y) |> Nx.to_scalar()
+    assert d == 9
+  end
+
+  test "chebyshev" do
+    {x, y} = get_xy()
+    d = chebyshev(x, y) |> Nx.to_scalar()
+    assert d == 3
+  end
+
+  test "minkowski" do
+    {x, y} = get_xy()
+    d = minkowski(x, y, 1.5) |> Nx.to_scalar()
+    assert_in_delta(d, 6.24025147, @delta)
+  end
+
+  test "wminkowski" do
+    {x, y} = get_xy()
+    d = wminkowski(x, y, 1.5, 0.5) |> Nx.to_scalar()
+    assert_in_delta(d, 3.12012573, @delta)
+  end
+
+  test "seuclidean" do
+    {x, y} = get_xy()
+    d = seuclidean(x, y, 0.5) |> Nx.to_scalar()
+    assert_in_delta(d, 7.34846923, @delta)
+  end
+
+  test "hamming" do
+    {x, y} = get_xy()
+    d = hamming(x, y) |> Nx.to_scalar()
+    assert d == 1
+  end
+
+  test "canberra" do
+    {x, y} = get_xy()
+    d = canberra(x, y) |> Nx.to_scalar()
+    assert_in_delta(d, 2.02857143, @delta)
+  end
+
+  test "braycurtis" do
+    {x, y} = get_xy()
+    d = braycurtis(x, y) |> Nx.to_scalar()
+    assert_in_delta(d, 0.6, @delta)
+  end
+
+  defp get_xy() do
+    x = Nx.tensor([[0, 1, 2]])
+    y = Nx.tensor([[3, 4, 5]])
+    {x, y}
+  end
+
+end
