7243d525fd12939ca3c641b07730327fe6d48e50
arpieb <rob@octobang.com>
2021-06-21 22:50:47 -0400

    #8: WIP; updated KDTree traversal to use metrics and hypersphere tests

--- lib/nixa/neighbors/kd_tree.ex
+++ lib/nixa/neighbors/kd_tree.ex
@@ -7,6 +7,7 @@ defmodule Nixa.Neighbors.KDTree do
 
   defstruct [
     metric_fn: nil,
+    metric_fn_args: nil,
     num_dims: nil,
     root: nil,
   ]
@@ -63,7 +64,8 @@ defmodule Nixa.Neighbors.KDTree do
   Create new KDTree from provided inputs using provided options
   """
   def create(inputs, opts \\ []) when is_list(inputs) do
-    metric_fn = Keyword.get(opts, :metric, :sqeuclidean)
+    metric_fn = Keyword.get(opts, :metric, {:minkowski, [2]})
+    metric_fn = if is_tuple(metric_fn), do: metric_fn, else: {metric_fn, []}
     num_dims = inputs |> hd() |> axis_size(1)
     root = kdtree(inputs, num_dims, 0)
 
@@ -79,30 +81,52 @@ defmodule Nixa.Neighbors.KDTree do
   """
   def query(tree, inputs, opts \\ []) do
     inputs
-    |> Enum.map(fn input -> Task.async(fn -> query_one(tree, input, opts) end) end)
-    |> Task.await_many()
+    |> Enum.map(fn input -> query_one(tree, input, opts) end)
   end
 
   ### Internal functions
 
-  defp query_one(tree, input, _opts) do
-    # TODO implement k-nearest logic
-    # k = Keyword.get(opts, :k, 1)
-    traverse_tree(tree.root, tree.num_dims, input, 0)
+  defp query_one(tree, input, opts) do
+    traverse_tree(tree.root, tree.num_dims, tree.metric_fn, Nx.squeeze(input), 0, opts)
     |> Nx.new_axis(0)
   end
 
-  defp traverse_tree(node, num_dims, input, depth) do
+  defp traverse_tree(node, num_dims, metric_fn, input, depth, opts) do
+    # TODO implement k-nearest logic
+    # k = Keyword.get(opts, :k, 1)
     f = rem(depth, num_dims)
-    cond do
-      comp_tensors(input[0][f], node.location[f], &Nx.less/2) and node.left -> traverse_tree(node.left, num_dims, input, depth + 1)
-      comp_tensors(input[0][f], node.location[f], &Nx.greater/2) and node.right -> traverse_tree(node.right, num_dims, input, depth + 1)
-      true -> node.location
+    {side, best} = cond do
+      comp_tensors(input[f], node.location[f], &Nx.less/2) and node.left ->
+        {:left, traverse_tree(node.left, num_dims, metric_fn, input, depth + 1, opts)}
+      comp_tensors(input[f], node.location[f], &Nx.greater/2) and node.right ->
+        {:right, traverse_tree(node.right, num_dims, metric_fn, input, depth + 1, opts)}
+      true -> {:nil, node.location}
+    end
+    if !side do
+      best
+    else
+      node_input_d = calc_metric(metric_fn, node.location, input)
+      best_input_d = calc_metric(metric_fn, best, input)
+      {best_dist, best} = if node_input_d < best_input_d, do: {node_input_d, node.location}, else: {best_input_d, best}
+      if calc_metric(metric_fn, input[f], node.location[f]) < best_dist do
+        poss_best = cond do
+          side == :left and node.right -> traverse_tree(node.right, num_dims, metric_fn, input, depth + 1, opts)
+          side == :right and node.left -> traverse_tree(node.left, num_dims, metric_fn, input, depth + 1, opts)
+          true -> best
+        end
+        if calc_metric(metric_fn, poss_best, input) < best_dist, do: poss_best, else: best
+      else
+        best
+      end
     end
   end
 
   defp comp_tensors(a, b, comp_fn) do
     (comp_fn.(a, b) |> Nx.to_scalar()) == 1
+  end
+
+  defp calc_metric({f, args}, x, y) do
+    apply(Nixa.Distance, f, [x, y] ++ args)
   end
 
   defp kdtree([], _k, _depth), do: nil
--- test/neighbors_test.exs
+++ test/neighbors_test.exs
@@ -3,9 +3,10 @@ defmodule NixaTest.Neighbors do
   doctest Nixa
 
   test "KDTree" do
-    num_samples = 101
+    num_samples = 11
     num_f = 3
-    inputs = Nx.random_uniform({num_samples, num_f}) |> Nx.to_batched_list(1)
+    raw_inputs = Nx.random_uniform({num_samples, num_f})
+    inputs = raw_inputs |> Nx.to_batched_list(1)
     tree = Nixa.Neighbors.KDTree.create(inputs)
     outputs = Nixa.Neighbors.KDTree.query(tree, inputs)
     num_matches = Nx.equal(Nx.concatenate(inputs), Nx.concatenate(outputs))
